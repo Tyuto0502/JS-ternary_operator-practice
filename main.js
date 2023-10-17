74ee92220e9ea27df4f11d9f1daee479aad4d7d4
Yuto Tamura <cayt1224@gmail.com>
2023-10-17 21:38:30 +0900

    課題作成

---
+++ main.js
@@ -0,0 +1,26 @@
+var boolean = true;
+var resultText = boolean ? "true処理" : "false処理";
+console.log(resultText);
+
+
+var x = 7;
+var num = x > 5;
+var resultNum = num ? "5より大きい" : "5より小さい";
+console.log(resultNum);
+
+
+var y = 3;
+var num = y < 5;
+var resultNum = num ? "5より小さい" : "5より大きい";
+console.log(resultNum);
+
+
+var xy = 12;
+var num = xy >= 10;
+var resultNum = num ? "10以上" : "10以下";
+console.log(resultNum);
+
+
+var boolean = " ";
+var resultText = boolean ? "true処理" : "false処理";
+console.log(resultText);
